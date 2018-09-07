699d5c511f10b01624c39096ef2435085208cc50
rbtprograms <rbt61690@gmail.com>
2018-09-07 13:39:22 -0700

    testing testing testing

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { updateGreeting, updateName } from './actions';
 
 class Controls extends PureComponent {
   state = {
@@ -8,22 +9,36 @@ class Controls extends PureComponent {
     name: ''
   }
 
-  handleChange = ({ target }) => {
+  static propTypes = {
+    updateGreeting: PropTypes.func,
+    updateName: PropTypes.func,
+  }
+
+  handleChangeName = ({ target }) => {
+    const { updateName } = this.props;
     this.setState({ [target.name]: target.value });
+    updateName(this.state.name);
+  }
+
+  handleChangeGreeting = ({ target }) => {
+    const { updateGreeting } = this.props;
+    this.setState({ [target.name]: target.value })
+    updateGreeting(this.state.greeting);
   }
   render() { 
     const { greeting, name } = this.state;
     return (
       <form>
-        <InputControl name="greeting" value={greeting} onChange={this.handleChange}/>
-        <InputControl name="name" value={name} onChange={this.handleChange}/>
+        <InputControl name="greeting" value={greeting} onChange={this.handleChangeGreeting}/>
+        <InputControl name="name" value={name} onChange={this.handleChangeName}/>
       </form>
     );
   }
 }
  
 export default connect(
-
+  null,
+  { updateGreeting, updateName }
 )(Controls);
 
 const InputControl = (props) => (
