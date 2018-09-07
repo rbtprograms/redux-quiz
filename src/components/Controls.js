86509f84af0dd9e1c72b9a8384f13db6f4c8a77a
rbtprograms <rbt61690@gmail.com>
2018-09-07 13:08:56 -0700

    starting to work

---
+++ src/components/Controls.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Controls extends PureComponent {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Controls;
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends PureComponent {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Display;
